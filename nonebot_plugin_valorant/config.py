216b2f94f179cbbba7c1fd989677b6c1dc822b31
BalconyJH <balconyjh@gmail.com>
2023-06-30 08:58:48 +0800

    🙈 移除config中的信息

--- nonebot_plugin_valorant/config.py
+++ nonebot_plugin_valorant/config.py
@@ -5,14 +5,13 @@ from pydantic import BaseSettings, Extra
 
 
 class Config(BaseSettings, extra=Extra.ignore):
-    valorant_language_type: str = "zh_CN"
-    valorant_database: str = "mysql+pymysql://root:070499@localhost:3306/valorant_bot"
+    valorant_database: str = ""
     valorant_database_key_path: str = None
-    valorant_proxies: str = "http://127.0.0.1:10809"
-    valorant_timeout: int = 30
+    valorant_proxies: str = ""
+    valorant_timeout: int
     valorant_to_me: bool = True
     valorant_command: Union[str, List[str]] = ""
-    language_type: str = "zh_cn"
+    language_type: str = ""
 
 
 global_config = get_driver().config
