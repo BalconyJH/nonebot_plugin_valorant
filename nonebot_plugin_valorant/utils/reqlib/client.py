9452fec5d7428c65d85c651398265219bceaeac5
BalconyJH <balconyjh@gmail.com>
2023-09-10 22:10:38 +0800

    🤡 瞎用异步卡了

--- nonebot_plugin_valorant/utils/reqlib/client.py
+++ nonebot_plugin_valorant/utils/reqlib/client.py
@@ -1,54 +1,64 @@
 from importlib.metadata import version
 
-from nonebot_plugin_valorant.utils.reqlib.request_res import base_url, get_request_json
+from nonebot_plugin_valorant.utils.reqlib.request_res import (
+    base_url,
+    get_request_json,
+    get_request_json_sync,
+)
 
 
-async def get_client_version() -> str:
+def get_client_version() -> str:
     """
     获取 Valorant 客户端版本信息。
 
     Returns:
         str: 客户端版本信息，格式为 "<分支>-shipping-<构建版本>-<第四位版本号>"。
     """
-    data = await get_request_json(url=base_url, sub_url="version")
-    return f"{data['data']['branch']}-shipping-{data['data']['buildVersion']}-{data['data']['version'].split('.')[3]}"
+    data = get_request_json_sync(url=base_url, sub_url="version")
+    return (
+        f"{data['data']['branch']}-shipping-{data['data']['buildVersion']}-"
+        f"{data['data']['version'].split('.')[3]}"
+    )
 
 
-async def get_version_data() -> dict:
-    data = await get_request_json(url=base_url, sub_url="version")
+def get_version_data() -> dict:
+    data = get_request_json_sync(url=base_url, sub_url="version")
     version_data = data.get("data", {})
     version_data.pop("status", None)
     return version_data
 
 
-async def get_valorant_version() -> str:
+def get_valorant_version() -> str:
     """
     获取 VALORANT 版本号。
 
     Returns:
         str: VALORANT 版本号。
     """
-    data = await get_request_json(url=base_url, sub_url="version")
+    data = get_request_json_sync(url=base_url, sub_url="version")
     return data["data"]["version"]
 
 
-async def get_manifest_id() -> str:
+def get_manifest_id() -> str:
     """
     获取最新的资源清单值。
 
     Returns:
         str: 资源清单值。
     """
-    data = await get_request_json(url=base_url, sub_url="version")
+    data = get_request_json_sync(url=base_url, sub_url="version")
     return data["data"]["manifestId"]
 
 
-async def get_bot_version() -> str:
+def get_bot_version() -> str:
     return version("nonebot_plugin_valorant")
 
 
 async def get_version() -> dict:
-    data = await get_request_json(url=base_url, sub_url="version")
+    data = await get_request_json(
+        url=base_url,
+        sub_url="version",
+    )
     return {
         _version: method() if callable(method) else method
         for _version, method in data["data"].items()
